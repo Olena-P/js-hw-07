e31687d48227b4d35eaec6cfc86b8e28f3bc473e
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-03-30 01:04:37 +0300

    change

--- task-1.js
+++ task-1.js
@@ -1,38 +1,7 @@
 /*
 В HTML есть список категорий ul#categories.
 
-<ul id="categories">
-  <li class="item">
-    <h2>Животные</h2>
 
-    <ul>
-      <li>Кот</li>
-      <li>Хомяк</li>
-      <li>Лошадь</li>
-      <li>Попугай</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Продукты</h2>
-
-    <ul>
-      <li>Хлеб</li>
-      <li>Петрушка</li>
-      <li>Творог</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Технологии</h2>
-
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>React</li>
-      <li>Node</li>
-    </ul>
-  </li>
-</ul>
 
 Напиши скрипт, который выполнит следующие операции.
 
