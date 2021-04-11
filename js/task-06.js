3653a391264569fea3ca1133eaa9cca43c49d695
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-04-11 20:41:37 +0300

    fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,23 +1,25 @@
 //Напиши скрипт, который бы при потере фокуса на инпуте, проверял его содержимое на правильное количество символов.
 
-const inputVal = document.querySelector('#validation-input');
-
-const lengthEl = inputVal.getAttribute('data-length');
-
-inputVal.addEventListener('blur', eventList);
-
 /*
 ecли введено подходящее количество, то border инпута становится зеленым, 
 если неправильное - красным.
 Для добавления стилей, используй CSS-классы valid и invalid.
 */
-function eventList() {
-  if (inputVal.value.length >= lengthEl) {
-    inputVal.setAttribute('class', 'valid');
-  } else if (inputVal.value === '') {
-    inputVal.setAttribute('class', '');
-  } else {
-    inputVal.setAttribute('class', 'invalid');
+let inputVal = document.getElementById("validation-input");
+
+let totalLenght = inputVal.getAttribute("data-length");
+let intTotallenght = parseInt(totalLenght, 10);
+
+inputVal.oninput = function() {
+  if (inputVal.value.length === intTotallenght) {
+    inputVal.classList.remove("invalid");
+    inputVal.classList.add("valid");
+  }
+  if (inputVal.value.length === 0) {
+    inputVal.classList.remove("valid");
+    inputVal.classList.remove("invalid");
+  }
+  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+    inputVal.classList.add("invalid");
   }
 };
-console.log(eventList);
